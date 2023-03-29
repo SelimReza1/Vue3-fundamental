ac75f89f98c0ebda6d1f4a516852b6b6ae72102c
SelimReza1 <selimreza9920@gmail.com>
2023-03-30 00:24:19 +0600

    binding

--- main.js
+++ main.js
@@ -3,7 +3,13 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            message: 'Hello Selim'
+            message: 'Hello Selim',
+            link: 'https://vuejs.org/guide/essentials/template-syntax.html#attribute-bindings',
+            htmlContent: '<h1>From Heading One</h1>',
+            objectOfAttrs: {
+                id: 'container',
+                class: 'wrapper'
+            }
         }
     }
 }).mount('#app')
