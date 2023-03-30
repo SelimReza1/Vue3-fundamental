c8d4fca4f57c600ea7b376997cef5d5da4113058
SelimReza1 <selimreza9920@gmail.com>
2023-03-30 11:02:15 +0600

    event handeling

--- main.js
+++ main.js
@@ -3,13 +3,16 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            message: 'Hello Selim',
-            link: 'https://vuejs.org/guide/essentials/template-syntax.html#attribute-bindings',
-            htmlContent: '<h1>From Heading One</h1>',
-            objectOfAttrs: {
-                id: 'container',
-                class: 'wrapper'
-            }
+            count: 0,
+            name: ''
         }
+    },
+    methods: {
+        onIncrementCounter(event, name) {
+            this.count = this.count + 1;
+            this.name = name;
+            console.log(event);
+        }
+
     }
 }).mount('#app')
