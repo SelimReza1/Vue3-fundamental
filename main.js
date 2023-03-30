4b9bd324880bd147f765984540694a26e769ec9c
SelimReza1 <selimreza9920@gmail.com>
2023-03-30 22:00:46 +0600

    list rendering using v-for

--- main.js
+++ main.js
@@ -3,7 +3,32 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-          isLoggedIN: true
+          isLoggedIN: true,
+            person: {
+              name: 'Selim',
+                Age: 26
+            },
+            posts: [
+                {
+                    title: 'Titel One',
+                    description: 'Post Description One'
+                },{
+                    title: 'Titel Two',
+                    description: 'Post Description Two'
+                },
+                {
+                    title: 'Titel Three',
+                    description: 'Post Description Three'
+                },
+                {
+                    title: 'Titel Four',
+                    description: 'Post Description Four'
+                },
+                {
+                    title: 'Titel Five',
+                    description: 'Post Description Five'
+                }
+            ]
         }
     },
     methods: {
