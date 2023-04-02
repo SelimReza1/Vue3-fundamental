85cf620e87d0f63cef5f5235278bbbdb35a9cd3a
SelimReza1 <selimreza9920@gmail.com>
2023-04-03 00:06:24 +0600

    introduction of component

--- main.js
+++ main.js
@@ -1,68 +1,23 @@
-const {createApp} = Vue;
 
-createApp({
+var app = Vue.createApp({
     data() {
         return {
-            colors: [],
-            color: 'red'
         }
     },
 
-    beforeCreate() {
-        console.log('before create');
-        console.log(this.color);
-        console.log(document.getElementById('color'));
-    },
+});
 
-    created() {
-        console.log('created');
-        console.log(this.color);
-        console.log(document.getElementById('color'));
-    },
+app.component('hobbies-component', {
+    template: ` <h5>Hobbies</h5>
+                <ul>
+                    <li v-for="hobby in hobbies"> {{ hobby }}</li>
+                </ul>`,
 
-    beforeMount() {
-        console.log('before mount');
-        console.log(this.color);
-        console.log(document.getElementById('color'));
-    },
-
-    mounted() {
-        console.log('mounted');
-        console.log(this.color);
-        console.log(document.getElementById('color'));
-    },
-
-    beforeUpdate() {
-        console.log('before update');
-    },
-
-    updated() {
-        console.log('updated');
-    },
-
-    beforeUnMount() {
-        console.log('before unmounted');
-    },
-
-    unMounted() {
-        console.log('unmounted');
-
-    },
-
-
-    methods: {
-        addItem() {
-            if (this.color) {
-                this.colors.push(this.color);
-                this.color = '';
-            } else {
-                console.log('empty');
-            }
-
-        },
-        deleteItem(index) {
-            this.colors.pop(index);
+    data() {
+        return {
+            hobbies: ['gardening', 'writing', 'listening'],
         }
     }
+});
 
-}).mount('#app')
+app.mount('#app');
