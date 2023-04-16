7251c54eeebca182aa94b9ede5432f5cb197ec9f
SelimReza1 <selimreza9920@gmail.com>
2023-04-17 00:18:45 +0600

    post and single post component with props

---
+++ src/components/Posts.vue
@@ -0,0 +1,26 @@
+<template>
+  <h2>Posts Data</h2>
+   <single-post v-for="post in posts" :key="post.title" :post="post" isActive="1"></single-post>
+</template>
+
+<script>
+import SinglePost from './SinglePost.vue'
+export default {
+  components: {
+    SinglePost
+  },
+  data() {
+    return {
+      posts: [
+        {title: 'Hello Bangladesh', description: 'Hello Bandladesh Description'},
+        {title: 'Hello USA', description: 'Hello USA Description'}
+
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/SinglePost.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+  <h4> {{ post.title }} </h4>
+  <h6> {{ post.description }} </h6>
+  <h6> {{ isActive }} </h6>
+</div>
+  <hr>
+</template>
+
+<script>
+export default {
+  data() {
+
+  },
+  props: ['post', 'isActive']
+}
+</script>
+
+<style>
+
+</style>
